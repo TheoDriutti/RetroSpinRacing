ed884c16536f4e973b06120f26554f0102da295d
Blue <mathieu.prevot@edu.devinci.fr>
2021-11-03 11:27:19 +0100

    bucketMinigame

---
+++ New Unity Project/Assets/UIManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance = null;
+
+    public int currentScore;
+    public Text scoreText;
+    public Image nextItem;
+    public Sprite[] imgs;
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScore = 0;
+    }
+
+    public void HandleScore()
+    {
+        scoreText.text = "Score: " + currentScore;
+    }
+    public void SetNextItem(int index)
+    {
+        nextItem.sprite = imgs[index];
+    }
+}
